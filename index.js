9955dfebd115ae6701a0457bf4eb4af697f303f0
JuanFelipeCB01 <jfelipecbermudez@gmail.com>
2023-07-26 10:09:55 +0200

    index y dotenv

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-//hola
+//Buenas noches
+
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.listen(PORT, ()=>console.log(`buenas noches ${PORT}`))
